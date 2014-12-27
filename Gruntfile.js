4ef2346ab0e029f7b83de1c47385024ed4c9768d
Daniel Schmidt <dschmidt@weluse.de>
2014-12-27 16:21:27 +0100

    deployment step

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+// Generated on 2013-07-01 using generator-angular 0.3.0
+'use strict';
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to recursively match all subfolders:
+// 'test/spec/**/*.js'
+
+module.exports = function (grunt) {
+  // load all grunt tasks
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  grunt.initConfig({
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '.',
+          dest: 'dist',
+          src: [
+            'index.html'
+          ]
+        }]
+      }
+    },
+    latex: {
+        options: {
+          haltOnError: 'true',
+          outputDirectory: 'dist/pdf'
+        },
+        dist: {
+          src: ['presentation.tex']
+        },
+    },
+    'gh-pages': {
+      options: {
+        base: 'dist'
+      },
+      src: ['**']
+    }
+
+  });
+
+  grunt.registerTask('default', [
+    'copy',
+    'gh-pages'
+  ]);
+};
